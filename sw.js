0dfe046d51fbb93b2a1fb7aa40488565b9f938e4
JiangBao <dakuairen@126.com>
2025-10-20 11:24:24 +0800

    Site updated: 2025-10-20 11:24:24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760929434327";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760930662369";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
